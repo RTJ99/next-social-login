efe68ab9b5c899db78263507ece1711e632223a1
rtj99 <ryantjena@gmail.com>
2023-08-24 18:38:44 +0200

    twitter login

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,15 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
-
+import TwitterProvider from "next-auth/providers/twitter";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_API_KEY ?? "",
+      clientSecret: process.env.TWITTER_API_KEY_SECRET ?? "",
     }),
   ],
 });
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="flex bg-[#222] min-h-screen flex-col items-center justify-between p-24">
       <div>
+        <h1 className="text-center font-bold text-3xl mb-5">Hey, There</h1>
         <LoginButton />
       </div>
     </main>
